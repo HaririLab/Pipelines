95f50530ea5d0ac65354b57c33403f4e655225d6
maxwe128 <maxwe128elliott@gmail.com>
2017-06-02 14:34:30 -0400

    Create segQAmontage.sh

---
+++ utils/segQAmontage.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+##################################segQAmontage.sh##############################
+####################### Authored by Max Elliott 6/2/2017 ####################
+
+####Description####
+#originally created to aid in QAing Freesurfer segmentations in the way Enigma suggests, makes a 2x2 montage of pial surface with segmentation loaded
+
+npb=13
+sumaDir=$1 ##Path to a subjects suma Directory in All_imaging
+image=${sumaDir}/std.60.lh.aparc.a2009s.annot.niml.dset
+surf=$(sumaDir}/std.60.FreeSurfer_both.spec
+scriptsDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+suma -niml -npb $npb -spec $surf &
+
+DriveSuma -npb $npb -com surf_cont -switch_surf lh.${mesh}
+DriveSuma -npb $npb -com viewer_cont -load_view ${scriptsDir}/${mesh}ViewLateral.niml.vvs
+DriveSuma -npb $npb -com viewer_cont -key F4 -com viewer_cont -key F5 -com viewer_cont -key F9
+
+DriveSuma -npb $npb  -com viewer_cont -key ctrl+left \
+	-com viewer_cont -key r
+DriveSuma -npb $npb -com  recorder_cont -save_as tmp1.png
+DriveSuma -npb $npb  -com viewer_cont -key ctrl+right \
+	-com viewer_cont -key r
+DriveSuma -npb $npb -com  recorder_cont -save_as tmp2.png
+
+DriveSuma -npb $npb -com viewer_cont -load_view ${scriptsDir}/${mesh}ViewMedial.niml.vvs
+DriveSuma -npb $npb  -com viewer_cont -key ctrl+right \
+	-com viewer_cont -key [ \
+	-com viewer_cont -key r
+DriveSuma -npb $npb -com  recorder_cont -save_as tmp3.png
+DriveSuma -npb $npb  -com viewer_cont -key ] \
+	-com viewer_cont -key [ \
+  -com viewer_cont -key ctrl+left \
+	-com viewer_cont -key r
+DriveSuma -npb $npb -com  recorder_cont -save_as tmp4.png
+  
+imcat -prefix $prefix -matrix 4 2 tmp*.png
+
+convert $prefix.ppm $prefix.png
+rm tmp*.png $prefix.ppm
+ 
